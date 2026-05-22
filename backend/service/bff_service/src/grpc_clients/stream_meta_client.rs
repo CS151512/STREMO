495b22263030f10487fe66c55309e00bfbefdc6c
Finnik <73883383+golkity@users.noreply.github.com>
2026-05-22 16:24:26 +0300

    Feat(grpc): init grpc client and imp module for auth client

---
+++ backend/service/bff_service/src/grpc_clients/stream_meta_client.rs
@@ -0,0 +1,23 @@
+use crate::utils::errors::AppError;
+use super::pb::stream_meta::{stream_meta_service_client::StreamMetaServiceClient,
+    GetLiveStreamsRequest, GetLiveStreamsResponse};
+use tonic::transport::Channel;
+
+#[derive(Clone)]
+pub struct StreamMetaGrpcClient {
+    client: StreamMetaServiceClient<Channel>,
+}
+
+impl StreamMetaGrpcClient {
+    pub async fn connect(url: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let client = StreamMetaServiceClient::connect(url).await?;
+        Ok(Self { client })
+    }
+
+    pub async fn get_live_streams(&mut self, limit: i32,
+        cursor: String) -> Result<GetLiveStreamsResponse, AppError> {
+        let request = tonic::Request::new(GetLiveStreamsRequest { limit, cursor });
+        let response = self.client.get_live_streams(request).await?;
+        Ok(response.into_inner())
+    }
+}
