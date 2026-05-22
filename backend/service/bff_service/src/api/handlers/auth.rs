fe681470443f5d7a766a780f75e6cc781afd0102
Finnik <73883383+golkity@users.noreply.github.com>
2026-05-22 14:44:43 +0300

    Feat(auth.rs): impl register_handler

--- backend/service/bff_service/src/api/handlers/auth.rs
+++ backend/service/bff_service/src/api/handlers/auth.rs
@@ -10,6 +10,7 @@ use crate::{
     AppState,
 };
 
+
 pub async fn login_handler(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<LoginRequest>,
@@ -30,5 +31,26 @@ pub async fn login_handler(
         access_token: grpc_res.access_token,
         refresh_token: grpc_res.refresh_token,
         expires_in: grpc_res.expires_in,
+    }))
+}
+
+
+pub async fn register_handler(
+    State(state):  State<Arc<AppState>>,
+    Json(payload): Json<RegisterRequest>,
+) -> Result<Json<RegisterResponse>, AppError> {
+    if payload.username .is_empty() {
+        return Err(AppError::BadRequest(
+            "Username is required".to_string()
+        ))
+    }
+
+    let mut auth_client = state.auth_grpc_client.clone();
+    let grpc_res = auth_client.register(payload.username,
+        payload.email, payload.password).await?;
+
+    Ok(Json(RegisterResponse {
+        id: grpc_res.id,
+        message: grpc_res.message,
     }))
 }
