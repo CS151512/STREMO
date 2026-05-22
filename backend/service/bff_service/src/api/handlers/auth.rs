ea1763c657e2877b723f2fe148b2bc5766a7d107
Finnik <73883383+golkity@users.noreply.github.com>
2026-05-22 14:29:46 +0300

    Feat(auth.rs): impl login handler and init crate with models

---
+++ backend/service/bff_service/src/api/handlers/auth.rs
@@ -0,0 +1,34 @@
+use axum::{extract::State, Json};
+use std::sync::Arc;
+
+use crate::{
+    models::{
+        requests::{LoginRequest, RegisterRequest},
+        responses::{LoginResponse, RegisterResponse},
+    },
+    utils::errors::AppError,
+    AppState,
+};
+
+pub async fn login_handler(
+    State(state): State<Arc<AppState>>,
+    Json(payload): Json<LoginRequest>,
+) -> Result<Json<LoginResponse>, AppError> {
+    if payload.email.is_empty() || payload.password.is_empty() {
+        return Err(AppError::BadRequest(
+            "Email and password are required".to_string()
+        ));
+    }
+
+    let mut auth_client =
+        state.auth_grpc_client.clone();
+
+    let grpc_res =
+        auth_client.login(payload.email, payload.password).await?;
+
+    Ok(Json(LoginResponse{
+        access_token: grpc_res.access_token,
+        refresh_token: grpc_res.refresh_token,
+        expires_in: grpc_res.expires_in,
+    }))
+}
