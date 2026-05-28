bf41bc5d09250c97054c1167067fa197a00ec099
Finnik <73883383+golkity@users.noreply.github.com>
2026-05-28 23:41:26 +0300

    Update(custom::errors): impl new struct for custom errors

--- backend/service/ingest-service/src/errors/custom.rs
+++ backend/service/ingest-service/src/errors/custom.rs
@@ -1,20 +1,11 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-#[allow(dead_code)]
 pub enum IngestError {
-    #[error("I/O Error: {0}")]
-    Io(#[from] std::io::Error),
-
-    #[error("Authentication Failed: {0}")]
-    AuthFailed(String),
-
-    #[error("FFmpeg Process Error: {0}")]
-    TranscoderError(String),
-
-    #[error("Client Disconnected")]
-    CliendDisconnected,
-
-    #[error("Internal Error: {0}")]
-    Internal(#[from] anyhow::Error),
+    #[error("Stream validation failed: {0}")]
+    Validation(String),
+    #[error("Authentication error: {0}")]
+    Auth(String),
+    #[error("Infrastructure error: {0}")]
+    Infra(#[from] anyhow::Error),
 }
